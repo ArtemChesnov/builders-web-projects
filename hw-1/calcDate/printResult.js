61c7e7016533b5a79ec82fefcfa1ba10d656beb7
Artem <jaksan37@gmail.com>
2022-09-03 09:52:17 +0300

    fix

--- hw-1/calcDate/printResult.js
+++
@@ -1,9 +0,0 @@
-const result = document.getElementById('datecalc__result')
-
-export const printError = (errorText) => {
-  result.innerText = errorText
-}
-
-export const printResult = ({ years, months, days }) => {
-  result.innerText = `Год: ${years} - Месяц: ${months} - День: ${days}`
-} 
--- hw-1/timer/printTimer.js
+++
@@ -1,5 +0,0 @@
-const result = document.getElementById('timer__result');
-
-export const printError = (errorText) => {
-  result.innerText = errorText;
-};
